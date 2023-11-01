76f0fdf426578fd9f2e193eae92cdcd6c11c48f2
hotgorod <hotgorod@gmail.com>
2023-11-01 11:58:55 +0000

    Pages added - HomePage, ContactsPage, Login and Register

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,19 @@
-import React, { Suspense} from 'react';
-import ContactList from 'components/ContactList/ContactList';
-import Filter from 'components/Filter/Filter';
-import ContactForm from './ContactForm/ContactForm.';
+import React, { Suspense } from 'react';
 import css from './App.module.css';
-import { useSelector } from 'react-redux';
+
 
 import { Route, Routes } from 'react-router-dom';
-
-
+import HomePage from './pages/HomePage';
 
 export const App = () => {
-  
-  const contacts = useSelector(state => state.contacts.contacts);
-  
-  const addContactToPhonebook = (contactName, contactNumber) => {
-    const isDuplicate = contacts.some(
-      contact => contact.contactName === contactName
-    );
 
-    if (isDuplicate) {
-      alert('The contact already exist');
-      return; 
-    }
-   
-  }
-   return (
-     <div className={css.container}>
-       <Suspense fallback="Loading...">
-         <Routes>
-           <Route
-             path="/"
-             element={
-               <>
-                 
-                 <ContactForm addContactCallback={addContactToPhonebook} />
-                 
-                 <Filter />
-                 <ContactList />
-               </>
-             }
-           />
-               </Routes>
-       </Suspense>
-     </div>
-   );
+  return (
+    <div className={css.container}>
+      <Suspense fallback="Loading...">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
-
-
--- src/components/pages/ContactsPage.jsx
+++ src/components/pages/ContactsPage.jsx
@@ -1,10 +1,28 @@
 
+import ContactForm from 'components/ContactForm/ContactForm.';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ContactsPage = () => {
+    const contacts = useSelector(state => state.contacts.contacts);
+
+    const addContactToPhonebook = (contactName, contactNumber) => {
+      const isDuplicate = contacts.some(
+        contact => contact.contactName === contactName
+      );
+
+      if (isDuplicate) {
+        alert('The contact already exist');
+        return;
+      }
+    };
   return (
     <div>
-     Contacts Page
+      <ContactForm addContactCallback={addContactToPhonebook} />
+      <Filter />
+      <ContactList />
     </div>
   );
 }
