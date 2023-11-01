fae836890e2885473221e140423c5cccf0e2435a
hotgorod <126808621+hotgorod@users.noreply.github.com>
2023-11-01 13:02:50 +0000

    Deploying to gh-pages from @ hotgorod/goit-react-hw-08-phonebook@cfb9dd2a47ccaaa714bd95987aea0fce6fb98dc1 🚀

---
+++ static/js/76.e8bc1679.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[76],{76:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Home Page"})}}}]);
+//# sourceMappingURL=76.e8bc1679.chunk.js.map
