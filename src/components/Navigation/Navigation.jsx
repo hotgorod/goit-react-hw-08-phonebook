0d054eb37506b58d0420c12ac5d584ad9a11e8ce
hotgorod <hotgorod@gmail.com>
2023-11-01 16:57:35 +0000

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { selectAuthIsSignedIn} from 'redux/auth.selectors';
+import { selectAuthIsSignedIn } from 'redux/auth.selectors';
 
 const Navigation = () => {
-    const isSignedIn = useSelector(selectAuthIsSignedIn);
-    console.log("isSignedIn:", isSignedIn);
-    
+  const isSignedIn = useSelector(selectAuthIsSignedIn);
+
   return (
     <header>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+
+      {isSignedIn ? (
+        <>
+          <NavLink to="/contacts">Contacts</NavLink>
+          <button>Log Out</button>
+        </>
+      ) : (
+        <>
+          <NavLink to="/register">Register</NavLink>
+          <NavLink to="/login">Login</NavLink>
+        </>
+      )}
     </header>
   );
-}
+};
 
 export default Navigation;
